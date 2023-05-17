8289ee6303b08e868e5cbaf87896d8ee85ffdc09
Kunle Oni <onikunle03@gmail.com>
2023-05-17 15:49:34 +0100

    react_task

---
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src="HolbertonLogo.jpg" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </div>
+      <hr />
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <hr />
+      <div className="App-footer">
+        <p>Copyright 2020 - holberton School</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
