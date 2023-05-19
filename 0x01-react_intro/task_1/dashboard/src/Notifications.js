5101ad66996ef5e629f141d44f6d0e84d6440671
Kunle Oni <onikunle03@gmail.com>
2023-05-19 22:11:08 +0100

    react

--- 0x01-react_intro/task_1/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_1/dashboard/src/Notifications.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import "./Notifications.css";
 
 function Notifications() {
--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { getFullYear, getFooterCopy } from "./utils";
 
 function App() {
   return (
@@ -10,10 +11,17 @@ function App() {
       <hr />
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email"></input>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password"></input>
+          <button>OK</button>
+        </form>
       </div>
       <hr />
       <div className="App-footer">
-        <p>Copyright 2020 - holberton School</p>
+        Copyright {getFullYear()} - {getFooterCopy()}
       </div>
     </div>
   );
