042a2e29f7f80bde9b4a52a694e298ed65ee9368
StephainG <onibukunoluwa@yahoo.com>
2023-05-22 22:24:07 +0100

    'react'

--- 0x02-react_props/task_0/dashboard/src/App/App.js
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -1,29 +1,20 @@
-import './App.css';
-import { getFullYear, getFooterCopy } from "./utils";
+import React from "react";
+import Notifications from "../Notifications/Notifications";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src="./HolbertonLogo.jpg" alt="Holberton Logo" />
-        <h1>School dashboard</h1>
+    <React.Fragment>
+      <Notifications />
+      <div className="App">
+        <Header />
+        <Login />
+        <Footer />
       </div>
-      <hr />
-      <div className="App-body">
-        <p>Login to access the full dashboard</p>
-        <form>
-          <label htmlFor="email">Email:</label>
-          <input type="email" name="email"></input>
-          <label htmlFor="password">Password:</label>
-          <input type="password" name="password"></input>
-          <button>OK</button>
-        </form>
-      </div>
-      <hr />
-      <div className="App-footer">
-        Copyright {getFullYear()} - {getFooterCopy()}
-      </div>
-    </div>
+    </React.Fragment>
   );
 }
 
