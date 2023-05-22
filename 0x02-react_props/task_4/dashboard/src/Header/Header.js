b203bed696c8f72011e1ea106e5dab2af4cfb440
StephainG <onibukunoluwa@yahoo.com>
2023-05-22 23:03:20 +0100

    'react'

---
+++ 0x02-react_props/task_4/dashboard/src/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <>
+      <div className="App-header">
+        <img src="./HolbertonLogo.jpg" alt="Holberton Logo" />
+        <h1>School dashboard</h1>
+      </div>
+    </>
+  );
+}
